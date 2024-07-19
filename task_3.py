19d403db9f60b72bf3746299f4689d4085351865
Sergey <asmcm88@gmail.com>
2024-07-19 19:45:47 +0300

    fin_work 2

--- task_3.py
+++ task_3.py
@@ -12,20 +12,23 @@ from pathlib import Path
 from random import randint as rnd
 from sys import argv
 import logging
+import argparse
 
 
 def find_func(start, stop, count):
-
     gues_number = rnd(start, stop)
-    for k in range(1, count + 1):
-        check_num = int(input('Введите число для угадывания: '))
-        if check_num == gues_number:
+    lst_pl_answers = []
+    args.player_name = input('Введите имя игрока: ')
+    for k in range(1, int(count) + 1):
+        args.check_num = int(input('Введите число для угадывания: '))
+        lst_pl_answers.append(args.check_num)
+        if args.check_num == gues_number:
             print('Угадали!')
             print(gues_number)
-            logger_text = f'{gues_number = }; result: Win'
+            logger_text = f'Игрок: {args.player_name} {gues_number = }; result: Win Ответы: {lst_pl_answers}'
             logger.info(logger_text)
             return True
-        elif check_num < gues_number:
+        elif args.check_num < gues_number:
             print('Больше!')
             print(f'Осталось попыток: {count - k}')
         else:
@@ -34,37 +37,21 @@ def find_func(start, stop, count):
     else:
         print('Вы исчерпали все попытки!')
         print(f'Загаданное число: {gues_number}!')
-        logger_text = f'{gues_number = }; result: Loose'
+        logger_text = f'Игрок: {args.player_name} {gues_number = }; result: Loose Ответы игрока: {lst_pl_answers}'
         logger.info(logger_text)
         return False
 
 
-def check_argv(argv_):
-    try:
-        argv_ = list(map(int, argv_))
-
-    except ValueError as e:
-        logging.error(e)
-    return argv_
-
-
 if __name__ == '__main__':
 
-    # print(Path.cwd())
     logger = logging.getLogger(__name__)
     logging.basicConfig(filename='task_3.txt', filemode='a', encoding='utf-8', level=logging.INFO)
-    start, stop, count = 1, 10, 3
-    argv = argv[1:]
-    argv = check_argv(argv)
-    # argv = list(map(int, argv[1:]))
-    match argv:
-        case [stop]:
-            stop = argv[0]
-        case [start, stop]:
-            start, stop = argv[0:]
-        case [start, stop, count]:
-            start, stop, count = argv[0:]
 
-    find_func(start, stop, count)
+    parser = argparse.ArgumentParser(
+        description='Создается диапазон чисел для игры "Угадайка" и количество попыток')
+    parser.add_argument('start', help='Нижняя граница')
+    parser.add_argument('stop', help='Верхняя граница ')
+    parser.add_argument('count', help='Количество попыток')
+    args = parser.parse_args()
 
-
+    find_func(int(args.start), int(args.stop), int(args.count))
