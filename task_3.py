ab492dc2effb44dba491cc46a634b72f2da638c0
Sergey <asmcm88@gmail.com>
2024-07-20 10:15:35 +0300

    fin_work 3

--- task_3.py
+++ task_3.py
@@ -19,7 +19,15 @@ def find_func(start, stop, count):
     lst_pl_answers = []
     args.player_name = input('Введите имя игрока: ')
     for k in range(1, count + 1):
-        args.check_num = int(input('Введите число для угадывания: '))
+        while True:
+            try:
+                args.check_num = input('Введите число для угадывания: ')
+                lst_pl_answers.append(args.check_num)
+                args.check_num = int(args.check_num)
+                break
+            except ValueError:
+                print(f'{args.player_name}, надо ввести целочисленное число')
+                logger.error(f'{args.player_name} ввел {args}')
         lst_pl_answers.append(args.check_num)
         if args.check_num == gues_number:
             print('Угадали!')
@@ -46,13 +54,12 @@ def find_func(start, stop, count):
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logging.basicConfig(filename='task_3.txt', filemode='a', encoding='utf-8', level=logging.INFO)
+
     parser = argparse.ArgumentParser(
         description='Создается диапазон чисел для игры "Угадайка" и количество попыток')
-    parser.add_argument('-s', '--start', help='Нижняя граница')
-    parser.add_argument('-f', '--stop', help='Верхняя граница ')
-    parser.add_argument('-c', '--count', help='Количество попыток')
-    try:
-        args = parser.parse_args()
-        find_func(args.start, args.stop, args.count)
-    except TypeError:
-        logger.error(f'Один из переданных вами параметров не является целочисленным числом Вы ввели: {args} ')
+    parser.add_argument('-s', '--start', help='Нижняя граница', type=int)
+    parser.add_argument('-f', '--stop', help='Верхняя граница ', type=int)
+    parser.add_argument('-c', '--count', help='Количество попыток', type=int)
+    args = parser.parse_args()
+    find_func(args.start, args.stop, args.count)
+
